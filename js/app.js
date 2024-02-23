8bf91aeaff2bd763e655a522884cd5b1c43fdb93
rewavestudios <predrag.milanovic@apeiron-edu.eu>
2024-02-23 05:35:07 +0100

    add: objects, reset button

--- js/app.js
+++ js/app.js
@@ -1,49 +1,71 @@
-function playGame(playerMove) {
-  const computerMove = pickComputerMove();
+ //Object - Score
+      let score = JSON.parse(localStorage.getItem('score')) || {
+        wins: 0,
+        losses: 0,
+        ties: 0
+      };
 
-  let result = '';
+      //Compare moves to get result
+      function playGame(playerMove) {
+        const computerMove = pickComputerMove();
 
-  if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose.';
-    } else if (computerMove === 'paper') {
-      result = 'You win.';
-    } else if (computerMove === 'scissors') {
-      result = 'Tie.';
-    }
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie.';
-    } else if (computerMove === 'paper') {
-      result = 'You lose.';
-    } else if (computerMove === 'scissors') {
-      result = 'You win.';
-    }
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win.';
-    } else if (computerMove === 'paper') {
-      result = 'Tie.';
-    } else if (computerMove === 'scissors') {
-      result = 'You lose.';
-    }
-  }
+        let result = '';
 
-  alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}`);
-}
+        if (playerMove === 'scissors') {
+          if (computerMove === 'rock') {
+            result = 'You lose.';
+          } else if (computerMove === 'paper') {
+            result = 'You win.';
+          } else if (computerMove === 'scissors') {
+            result = 'Tie.';
+          }
+        } else if (playerMove === 'rock') {
+          if (computerMove === 'rock') {
+            result = 'Tie.';
+          } else if (computerMove === 'paper') {
+            result = 'You lose.';
+          } else if (computerMove === 'scissors') {
+            result = 'You win.';
+          }
+        } else if (playerMove === 'paper') {
+          if (computerMove === 'rock') {
+            result = 'You win.';
+          } else if (computerMove === 'paper') {
+            result = 'Tie.';
+          } else if (computerMove === 'scissors') {
+            result = 'You lose.';
+          }
+        }
+        console.log(score);
 
-function pickComputerMove () {
-  const randomNumber = Math.random();
-    
-  let computerMove = '';
+        //Update a score
+        if (result === 'You win.') {
+          score.wins += 1;
+        } else if (result === 'You lose.') {
+          score.losses += 1;
+        } else if (result === 'Tie.') {
+          score.ties += 1;
+        }
 
-  if (randomNumber >=0 && randomNumber < 1 / 3) {
-    computerMove = 'rock';
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = 'paper';
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = 'scissors';
-  }
+        //Display result and score in a popup
+        alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
+        Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
+      }
 
-  return computerMove;
-}
+      localStorage.setItem('score', JSON.stringify(score));
+
+      //Computer randomly selects a move
+      function pickComputerMove () {
+        const randomNumber = Math.random();
+          
+        let computerMove = '';
+
+        if (randomNumber >=0 && randomNumber < 1 / 3) {
+          computerMove = 'rock';
+        } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+          computerMove = 'paper';
+        } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+          computerMove = 'scissors';
+        }
+        return computerMove;
+      }
